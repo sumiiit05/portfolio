5006652ca7e9d0721efc741f4edaceb0f4ff0bcf
sumit raut <rautsumit005@gmail.com>
2024-09-24 04:15:23 +0530

    nineth

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -11,8 +11,8 @@ const Parallax = ({ type }) => {
   });
 
   // Adjusted ranges to keep text and background within a reasonable movement
-  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
 
   return (
     <div
